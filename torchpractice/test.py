4366afa5f298378ec611a8fb6aa9400df0200751
3like3beer <les.ruiz@gmail.com>
2018-04-04 20:12:09 +0200

    Update test.py

--- torchpractice/test.py
+++ torchpractice/test.py
@@ -1,1 +1,14 @@
+from unittest import TestCase
+from __future__ import print_function
+import torch
+
+
+class TestSystem(TestCase):
+    def setUp(self):
+        
+
+    def test_get_availability(self):
+        x = torch.Tensor(5, 3)
+        print(x)
+        pass
 
